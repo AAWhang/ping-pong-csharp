9316c3e96c8c77e35f579651265a6c269743606f
Aaron Whang and Holden Clark <aaron@>
2019-07-15 09:55:16 -0700

    add queen attack method

--- Ping-pong.cs
+++ Ping-pong.cs
@@ -4,13 +4,15 @@ class Program
 {
   static void Main()
   {
-    Console.WriteLine("1: Ping-Pong, 2: Sphynx");
+    Console.WriteLine("1: Ping-Pong, 2: Sphynx, 3: QueenCheck");
 
     string progChoice = Console.ReadLine();
     if(progChoice == "1"){
       PingPong();
     } else if(progChoice == "2"){
       Sphynx();
+    } else if(progChoice == "3"){
+      Queens();
     }
   }
 
@@ -74,6 +76,40 @@ class Program
       return lose;
     }
 
+  }
+
+  static void Queens()
+  {
+    Console.WriteLine("Enter Queen Position!");
+    Console.WriteLine("Queen X:");
+    int queenX = int.Parse(Console.ReadLine());
+    Console.WriteLine("Queen Y:");
+    int queenY = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter Pawn Position!");
+    Console.WriteLine("Pawn X:");
+    int pawnX = int.Parse(Console.ReadLine());
+    Console.WriteLine("Pawn Y:");
+    int pawnY = int.Parse(Console.ReadLine());
+    Console.WriteLine(queenX);
+    int xDiag = negativeCheck(queenX,pawnX);
+    int yDiag = negativeCheck(queenY,pawnY);
+
+    if (queenX == pawnX || queenY == pawnY || xDiag == yDiag) {
+      Console.WriteLine("Queen takes Pawn!");
+    } else {
+      Console.WriteLine("Queen stares longingly at pawn");
+    }
+
+
+  }
+
+  static int negativeCheck(int num1, int num2)
+  {
+    if (num1 > num2){
+      return num1 - num2;
+    } else {
+      return num2 - num1;
+    }
   }
 
 
