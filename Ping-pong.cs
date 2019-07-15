643ef34db53fae62fe3511f080714ed52cb368e5
Aaron Whang and Holden Clark <holden@>
2019-07-15 09:25:48 -0700

    add sphynx riddle questions

--- Ping-pong.cs
+++ Ping-pong.cs
@@ -4,6 +4,18 @@ class Program
 {
   static void Main()
   {
+    Console.WriteLine("1: Ping-Pong, 2: Sphynx");
+
+    string progChoice = Console.ReadLine();
+    if(progChoice == "1"){
+      PingPong();
+    } else if(progChoice == "2"){
+      Sphynx();
+    }
+  }
+
+  static void PingPong()
+  {
     Console.WriteLine("Enter number to ping-pong to:");
     string userInput = Console.ReadLine();
     int chosenNum = int.Parse(userInput);
@@ -20,4 +32,49 @@ class Program
       }
     }
   }
+
+  static void Sphynx()
+  {
+    string result = SphynxQuestions();
+    Console.WriteLine(result);
+    Console.ReadLine();
+
+  }
+
+  static string SphynxQuestions()
+  {
+    string win = "you survive!";
+    string lose = "You died!";
+    Console.WriteLine("Answer the riddles correctly to survive!");
+    Console.WriteLine("What is the creature that walks on four legs in the morning, two legs at noon and three in the evening?");
+    string answer1 = Console.ReadLine().ToLower();
+    if(answer1 == "man"){
+      Console.WriteLine("Correct...");
+    } else {
+      Console.WriteLine("WRONG! *The Sphynx eats you whole*");
+      return lose;
+    }
+
+    Console.WriteLine("What is a man?");
+    string answer2 = Console.ReadLine().ToLower();
+    if(answer2 == "a miserable pile of secrets"){
+      Console.WriteLine("Correct...");
+    } else {
+      Console.WriteLine("WRONG! *The Sphynx eats you whole*");
+      return lose;
+    }
+
+    Console.WriteLine("Say yes");
+    string answer3 = Console.ReadLine().ToLower();
+    if(answer3 == "yes"){
+      Console.WriteLine("Correct... *Sphynx commits sudoku*");
+      return win;
+    } else {
+      Console.WriteLine("WRONG! *The Sphynx eats you whole*");
+      return lose;
+    }
+
+  }
+
+
 }
